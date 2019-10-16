e689b693a556e14d78bbbfffdb5cf3023e019ede
Getoari <getoari_g@hotmail.com>
2019-10-16 04:47:55 +0200

    admin master page setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,19 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([
+        'public/css/libs/bootstrap.css',
+        'public/css/libs/blog-post.css',
+        'public/css/libs/font-awesome.css',
+        'public/css/libs/metisMenu.css',
+        'public/css/libs/sb-admin-2.css',
+        'public/css/libs/styles.css'
+    ], 'public/css/libs.css')
+    .scripts([
+        'public/js/libs/jquery.js',
+        'public/js/libs/bootstrap.js',
+        'public/js/libs/metisMenu.js',
+        'public/js/libs/sb-admin-2.js',
+        'public/js/libs/scripts.js'
+    ], 'public/js/libs.js');
